c53c9f28bd76c4ae4fe5c4d72c92de2a3133db16
jassingh07 <91901835+jassingh07@users.noreply.github.com>
2023-04-04 00:34:40 +0530

    Added solution - LeetHub

---
+++ Minimum Steps Required - GFG/minimum-steps-required.cpp
@@ -0,0 +1,31 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function Template for C++
+
+int minSteps(string str) {
+    int res = 1;
+    for(int i = 1; i < str.length(); i++)
+        res += str[i] != str[i - 1];
+    return res / 2 + 1;
+}
+
+//{ Driver Code Starts.
+
+int main() {
+
+    int t;
+    cin >> t;
+    while (t--) {
+        string str;
+        cin >> str;
+        cout << minSteps(str) << endl;
+    }
+}
+
+// } Driver Code Ends
